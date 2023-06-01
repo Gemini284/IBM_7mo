35eba7483757f280171165ec22a380c5214c521d
Mafer <A01634135@tec.mx>
2023-06-01 16:26:06 -0600

    Mover dashboard a web-app

--- web-app/src/assets/animation.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Lottie from "lottie-react";
-import groovyWalkAnimation from "../assets/programming.json";
-
-const animation = () => <Lottie animationData={groovyWalkAnimation} loop={true} />;
-
-export default animation;
--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+// Material Dashboard 2 React Context Provider
+import { MaterialUIControllerProvider } from "context";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
+  </BrowserRouter>
 );
-
