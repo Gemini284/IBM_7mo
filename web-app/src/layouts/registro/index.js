605332387d12ea25249e275bb0fbb24575211d05
Sofia Batiz <A01634125@tec.mx>
2023-06-05 21:52:20 -0600

    creación de registro

---
+++ web-app/src/layouts/registro/index.js
@@ -0,0 +1,11 @@
+import Grid from "@mui/material/Grid";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
