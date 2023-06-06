e4af5bd8cdf1dd193c85818af8fb8ceae4454d8e
Sofia Batiz <A01634125@tec.mx>
2023-06-05 18:30:33 -0600

    arreglo del footer

--- web-app/src/examples/Footer/index.js
+++ web-app/src/examples/Footer/index.js
@@ -14,9 +14,11 @@ Coded by www.creative-tim.com
 */
 
 // prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
 
 // @mui material components
 import Link from "@mui/material/Link";
+import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -49,7 +51,29 @@ function Footer({ company, links }) {
       alignItems="center"
       px={1.5}
     >
-      
+      <MDBox
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        color="text"
+        fontSize={size.sm}
+        px={1.5}
+      >
+        &copy; {new Date().getFullYear()}, made with
+        <MDBox fontSize={size.md} color="text" mb={-0.5} mx={0.25}>
+          <Icon color="inherit" fontSize="inherit">
+            favorite
+          </Icon>
+        </MDBox>
+        by
+        <Link href={href} target="_blank">
+          <MDTypography variant="button" fontWeight="medium">
+            &nbsp;{name}&nbsp;
+          </MDTypography>
+        </Link>
+        for a better web.
+      </MDBox>
       <MDBox
         component="ul"
         sx={({ breakpoints }) => ({
@@ -74,5 +98,20 @@ function Footer({ company, links }) {
 }
 
 // Setting default values for the props of Footer
+Footer.defaultProps = {
+  company: { href: "https://www.creative-tim.com/", name: "Creative Tim" },
+  links: [
+    { href: "https://www.creative-tim.com/", name: "Creative Tim" },
+    { href: "https://www.creative-tim.com/presentation", name: "About Us" },
+    { href: "https://www.creative-tim.com/blog", name: "Blog" },
+    { href: "https://www.creative-tim.com/license", name: "License" },
+  ],
+};
+
+// Typechecking props for the Footer
+Footer.propTypes = {
+  company: PropTypes.objectOf(PropTypes.string),
+  links: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default Footer;
