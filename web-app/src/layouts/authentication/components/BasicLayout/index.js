05c952c5d8968aa8329cbf053249c8d6bb28dbc3
Mafer <A01634135@tec.mx>
2023-06-13 23:54:28 -0600

    cambiar imagen login

--- web-app/src/layouts/authentication/components/BasicLayout/index.js
+++ web-app/src/layouts/authentication/components/BasicLayout/index.js
@@ -23,8 +23,8 @@ function BasicLayout({ image, children }) {
           backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
             image &&
             `${linearGradient(
-              rgba(gradients.dark.main, 0.6),
-              rgba(gradients.dark.state, 0.6)
+              rgba(gradients.info.main, 0),
+              rgba(gradients.info.state, 0)
             )}, url(${image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
